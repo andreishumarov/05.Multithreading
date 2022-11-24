fbb960f76419d2ae11d6c76ca9df7e9c2d8cae7f
andreishumarov <114391036+andreishumarov@users.noreply.github.com>
2022-11-24 11:51:06 +0200

    Add files via upload

---
+++ static method/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace static_method
+{
+    public class MyThread
+    {
+        public static void Thread1()
+        {
+            for (int i = 0; i < 10; i++)
+                {
+                Console.WriteLine(i);
+            }
+        }
+    }
+    public class ThreadExample
+    {
+        public static void Main()
+        {
+            Thread t1 = new Thread(new ThreadStart(MyThread.Thread1));
+            Thread t2 = new Thread(new ThreadStart(MyThread.Thread1));
+            t1.Start();
+            t2.Start();
+        }
+    }
+}
